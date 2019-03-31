5a5b94113b1704ac5ab17cf3ae8a99bdd0c9c507
Xun Zhang <zx59812282@gmail.com>
2019-03-31 10:37:57 +0800

    client: add all services and a super loop (#6)
    add all services and a super loop to prompt the user enter. (exit
    not supported)
    
    Signed-off-by: Xun Zhang <zx59812282@gmail.com>

--- client/base_service.c
+++ client/base_service.c
@@ -1,57 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 #include "service.h"
-
-int ipc_tx(struct requestor* rq, const char* msg){
-    
-    sendto(rq->sockfd, (const char *)msg, strlen(msg),
-           MSG_DONTROUTE, (const struct sockaddr *) rq->servaddr,
-           sizeof(struct sockaddr) );
-    printf("Hello message sent.\n");
-
-    return 0;
-}
-
-int ipc_rx(struct requestor* rq, char* recv){
-    int n, len;
-    n = recvfrom(rq->sockfd, (char *)recv, MAXLINE,
-                 MSG_WAITALL, (struct sockaddr *) rq->servaddr,
-                 &len);
-    
-    recv[n] = '\0';
-    printf("Server : %s\n", recv);
-}
-
-char* ipc_cat(char* dest, const char* src){
-
-    int lend = strlen(dest);
-    int lens = strlen(src);
-    // printf("%d%d",lens,lend);
-    dest[lend] = (char) lens;
-    dest[lend+1] = '\0';
-
-    return strcat(dest, src);
-}
-
-int ipc_concat(int num, char* dest, ...){
- 
-    va_list valist;
-    char* tmp;
- 
-    va_start(valist, dest);
- 
-    for (int i = 0; i < num; i++)
-    {
-       tmp = va_arg(valist, char*);
-       ipc_cat(dest,tmp);
-    }
-    
-    va_end(valist);
- 
-    return 0;
-}
 
 static int base_s1(struct requestor* rq){
 
@@ -76,11 +26,158 @@ static int base_s1(struct requestor* rq){
     return 0;
 }
 
+static int base_s2(struct requestor* rq){
+    char msg[MAXLINE];
+    char buffer[MAXLINE];
+    char arg1[ARG_LENGTH];
+
+    memset(msg, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
+
+    printf("Flight number you want to enquire: ");
+    scanf("%s",arg1);
+
+    ipc_concat(5,msg,"0",rq->name,"12","2",arg1);
+
+    ipc_tx(rq, msg);
+    ipc_rx(rq, buffer);
+    
+    return 0;
+}
+
+static int base_s3(struct requestor* rq){
+    char msg[MAXLINE];
+    char buffer[MAXLINE];
+    char arg1[ARG_LENGTH];
+    char arg2[ARG_LENGTH];
+
+    memset(msg, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
+
+    printf("Flight number you want to book: ");
+    scanf("%s",arg1);
+    printf("Quantity (positive integer): ");
+    /* need check here */
+    scanf("%s",arg2);
+
+    ipc_concat(6,msg,"0",rq->name,"11","3",arg1,arg2);
+
+    ipc_tx(rq, msg);
+    ipc_rx(rq, buffer);
+    
+    return 0;
+}
+
+
+static int base_s4(struct requestor* rq){
+    char msg[MAXLINE];
+    char buffer[MAXLINE];
+    char arg1[ARG_LENGTH];
+    char arg2[ARG_LENGTH];
+
+    memset(msg, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
+
+    printf("Flight number you want to monitor: ");
+    scanf("%s",arg1);
+    printf("How long do you want to monitor? [10, 120] second: ");
+    /* need check here */
+    scanf("%s",arg2);
+
+    ipc_concat(6,msg,"0",rq->name,"10","4",arg1,arg2);
+
+    ipc_tx(rq, msg);
+    ipc_rx(rq, buffer);
+    
+    return 0;
+}
+
+
+static int base_s5(struct requestor* rq){
+    char msg[MAXLINE];
+    char buffer[MAXLINE];
+
+    memset(msg, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
+
+    printf("wait a second, proceeding...");
+
+    ipc_concat(4,msg,"0",rq->name,"9","5");
+
+    ipc_tx(rq, msg);
+    ipc_rx(rq, buffer);
+    
+    return 0;
+}
+
+
+static int base_s6(struct requestor* rq){
+    char msg[MAXLINE];
+    char buffer[MAXLINE];
+    char arg1[ARG_LENGTH];
+    char arg2[ARG_LENGTH];
+
+    memset(msg, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
+
+    printf("Flight number you want to cancel: ");
+    scanf("%s",arg1);
+    printf("Quantity (positive integer): ");
+    /* need check here */
+    scanf("%s",arg2);
+
+    ipc_concat(6,msg,"0",rq->name,"12","6",arg1,arg2);
+
+    ipc_tx(rq, msg);
+    ipc_rx(rq, buffer);
+    
+    return 0;
+}
+
 const struct serv_ops base_serv_ops = {
     .s1 = base_s1,
+    .s2 = base_s2,
+    .s3 = base_s3,
+    .s4 = base_s4,
+    .s5 = base_s5,
+    .s6 = base_s6,
 };
 
+static int base_call(int sn, struct requestor* rq){
+
+    struct serv_ops* ops = &base_serv_ops;
+
+    switch (sn)
+    {
+        case SER_FIND:
+            base_s1(rq);
+            break;
+        case SER_GETDETAIL:
+            base_s2(rq);
+            break;
+        case SER_BOOK:
+            base_s3(rq);
+            break;
+        case SER_MONITOR:
+            base_s4(rq);
+            break;
+        case SER_ORDERINFO:
+            base_s5(rq);
+            break;
+        case SER_CANCEL:
+            base_s6(rq);
+            break;
+        case SER_EXIT:
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
+
 int init_service(struct serv* s){
+    s->call_service = base_call;
     return 0;
 }
 
